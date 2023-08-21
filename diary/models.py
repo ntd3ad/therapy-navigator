f34bd8f4bba4489925708a1e617d948f488fae58
Полина Васильева <plinavasileva@gmail.com>
2023-08-21 22:35:01 +0300

    Добавила менеджер для опубликованных постов

--- diary/models.py
+++ diary/models.py
@@ -1,6 +1,13 @@
 from django.db import models
+from django.db.models.query import QuerySet
 from django.utils import timezone
 from django.contrib.auth.models import User
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status = Post.Status.PUBLISHED)
+
 
 class Post(models.Model):
     
@@ -14,6 +21,9 @@ class Post(models.Model):
     publish = models.DateTimeField(default=timezone.now)
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='diary_posts')
+    
+    objects = models.Manager()
+    published = PublishedManager()
     
     class Meta:
         ordering = ['-publish']
