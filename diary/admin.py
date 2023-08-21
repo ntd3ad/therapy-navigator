00a5b27c4c22394c123eb6259c0f480a1d86835d
Полина Васильева <plinavasileva@gmail.com>
2023-08-21 22:34:08 +0300

    Добавила посты в админку

--- diary/admin.py
+++ diary/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from diary.models import Post
 # Register your models here.
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'publish', 'status']
+    list_filter = ['status', 'publish', 'author']
+    search_fields = ['title', 'body']
+    raw_id_fields = ['author']
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
